fb422025b5319d8e0e020da41390469b4425f750
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:25:33 -0700

    added docstrings to first 4 functions

--- arithmetic.py
+++ arithmetic.py
@@ -1,13 +1,17 @@
 def add(num1, num2):
+    """ adds two numbers together"""
     return num1 + num2
 
 def subtract(num1, num2):
+    """subtracts one number from another"""
     return num1 - num2
 
 def multiply(num1, num2):
+    """multiplies two numbers"""
     return num1 * num2
 
 def divide(num1, num2):
+    """divides one number by another"""
     return float(num1)/num2
 
 def square(num1):
